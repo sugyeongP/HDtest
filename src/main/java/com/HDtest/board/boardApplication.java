d6dda3c512bbf276ea217f637837703c69f2494d
sugyeongpark <narangsae@gmail.com>
2022-02-11 17:18:52 +0900

    1,2단계. h2database가 되지않아 기존 oracle developer사용했습니다.

---
+++ src/main/java/com/HDtest/board/boardApplication.java
@@ -0,0 +1,13 @@
+package com.HDtest.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class boardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(boardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HDtest/board/boardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HDtest.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class boardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
